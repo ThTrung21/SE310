dd35fd48598509a94a450e56c66cb621df96674f
nguyenthucbao <21520617@gm.uit.edu.vn>
2024-12-22 14:32:10 +0700

    update import

---
+++ BookMK/Service/Temp/ImportService.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BookMK.Service
+{
+    public static class ImportService
+    {
+        public static readonly DependencyProperty SelectedItemsProperty =
+            DependencyProperty.RegisterAttached(
+                "SelectedItems",
+                typeof(IList),
+                typeof(ImportService),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, SelectedItemsChanged));
+
+        public static IList GetSelectedItems(DependencyObject obj)
+        {
+            return (IList)obj.GetValue(SelectedItemsProperty);
+        }
+
+        public static void SetSelectedItems(DependencyObject obj, IList value)
+        {
+            obj.SetValue(SelectedItemsProperty, value);
+        }
+
+        private static void SelectedItemsChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            if (obj is ListBox listBox)
+            {
+                listBox.SelectionChanged += (sender, args) =>
+                {
+                    SetSelectedItems(listBox, listBox.SelectedItems);
+                };
+            }
+        }
+    }
+}
